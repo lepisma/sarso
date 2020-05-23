62838e9554b21e367d3bffbd976897d44c64610c
Abhinav Tushar <abhinav@lepisma.xyz>
2020-05-23 21:13:11 +0530

    commandline parsing

--- src/sarso-sync.go
+++ src/sarso-sync.go
@@ -3,12 +3,27 @@ package main
 import (
 	// "database/sql"
 	"fmt"
+	"github.com/jessevdk/go-flags"
 	// _ "github.com/mattn/go-sqlite3"
 	jira "gopkg.in/andygrunwald/go-jira.v1"
 	"os"
 )
 
+func initDb(dbPath string) {
+	fmt.Println(":: Initializing database")
+}
+
+var opts struct {
+	DbPath      string `short:"f" long:"file" description:"Database file" value-name:"FILE" required:"true"`
+	ProjectName string `short:"p" long:"project" description:"Name of the project to sync" required:"true"`
+}
+
 func main() {
+	_, err := flags.Parse(&opts)
+	if err != nil {
+		panic(err)
+	}
+
 	base := os.Getenv("JIRA_BASE")
 	username := os.Getenv("JIRA_USER")
 	token := os.Getenv("JIRA_TOKEN")
@@ -18,11 +33,12 @@ func main() {
 		Password: token,
 	}
 
-	client, err := jira.NewClient(tp.Client(), base)
+	_, err = jira.NewClient(tp.Client(), base)
 	if err != nil {
 		panic(err)
 	}
 
-	// argsWithoutProg := os.Args[1:]
-	fmt.Print(client)
+	if _, err := os.Stat(opts.DbPath); os.IsNotExist(err) {
+		initDb(opts.DbPath)
+	}
 }
